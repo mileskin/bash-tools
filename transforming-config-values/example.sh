41b9c781ff24c565962ca245f413372580820063
Miki Leskinen <miki.leskinen@reaktor.fi>
2011-09-26 20:19:41 +0300

    Show current output in source file

--- transforming-config-values/example.sh
+++ transforming-config-values/example.sh
@@ -6,4 +6,5 @@ echo "hosts: '${hosts}'"
 transformedHosts=$(echo ${hosts} | sed 's/[^ ][^ ]*/--host=&/g')
 echo "transformedHosts: '${transformedHosts}'"
 echo "some-command ${transformedHosts}"
+# => some-command --host=example1.some.domain --host=example2.some.domain --host=example3.some.domain
 
